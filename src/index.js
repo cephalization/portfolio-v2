9f2860392fa10c8666074edee87c2d568224ff21
Tony Powell <adpowell@mtu.edu>
2017-07-17 21:57:39 -0400

    Include Semantic-UI project in portfolio-v2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 // Component Imports
 import MainComponent from './components/MainComponent';
 // Style Imports
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 // Other Imports
 import registerServiceWorker from './registerServiceWorker';
